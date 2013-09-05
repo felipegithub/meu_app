c8293272a31297dfd982ded3a2d4644509d991fc
felipegithub <felipefs92@gmail.com>
2013-09-05 09:05:21 -0300

    deploy acai

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require "bundler/capistrano"
 require "rvm/capistrano"
 
-server "10.12.0.69", :web, :app, :db, primary: true
+server "10.12.0.17", :web, :app, :db, primary: true
 
 set :application, "meu_app" # Nome da app
 set :user, "fabsoft" # usuario do SERVIDOR
@@ -10,12 +10,12 @@ set :use_sudo, false # Não executar os Comandos com 'sudo' no inicio
 
 set :scm, "git" # Tipo: git, bitcucket...
 set :repository, "https://github.com/felipegithub/meu_app.git" #URL do repositorio
-set :branch, "master"
+set :branch, "master" # Ramo(se você tem um versionamento de seu projeto no repositorio)
 
-set :rvm_ruby_string, :local # use the same ruby as used locally for deployment
-set :rvm_autolibs_flag, "read-only"  
+set :rvm_ruby_string, :local # use o mesmo ruby usado no ambiente de desenvolvimento
+set :rvm_autolibs_flag, "read-only"  # propriedade do rvm de instalar certas dependencias como OpenSSL por exemplo
 
-set :rails_env, "production"
+set :rails_env, "production" # ambiente definido para deploy
 
 #Parametros de conexão via ssh:
 default_run_options[:pty] = true
@@ -24,21 +24,12 @@ ssh_options[:forward_agent] = true
 after "deploy", "deploy:cleanup"
 
 #Definição das 'Tarefas para Deploy:'
-namespace :deploy do
 
-	#Cria-se tarefas, como se fossem metodos que vão ser executados conforme o deploy ocorre
-	task :setup_inicial_nginx, roles: :app do
-		sudo "ln -nfs #{current_path}/config/nginx_app.conf /etc/nginx/sites-enabled/#{application}"
-		sudo "service nginx restart"
-		puts "Criou o Link do arquivo de configuração do ngingx"
+
+namespace :diga do
+	task :ola_mundo, roles: :app do
+		puts "Ola mundo"
 	end
-
-	task :start_server, roles: :app do
-		run "cd #{current_path}; rails s -e production"
-	end
-
-	after "deploy:setup", "deploy:setup_inicial_nginx"
-	after "deploy:cold", "deploy:start_server"
-	after "deploy", "deploy:start_server"
-	
 end
+
+
